c89e36dd83604f1d2fd66ed61bbb49dc972f0aba
Andre Marcio <andre.marcio.ti@gmail.com>
2026-06-15 21:26:57 -0300

    verify task

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ def main() -> None:
     with open("./data/players.json") as f:
         players_data = json.load(f)
 
-
     for player_data in players_data:
         race, _ = Race.objects.get_or_create(name=player_data["race"])
 
-        guild_data = player_data.get('guild')
+        guild_data = player_data.get("guild")
         if guild_data:
             guild, _ = Guild.objects.get_or_create(name=guild_data)
         else:
@@ -23,10 +22,10 @@ def main() -> None:
 
         Player.objects.create(
             nickname=player_data["nickname"],
-            email = player_data["email"],
-            bio = player_data.get("bio", ''),
-            race = race,
-            guild = guild)
+            email=player_data["email"],
+            bio=player_data.get("bio", ""),
+            race=race,
+            guild=guild)
 
 
 if __name__ == "__main__":
