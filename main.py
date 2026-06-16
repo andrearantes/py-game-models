42c7634e4042fb6093a73a96e203a45835efbdbc
Andre Marcio <andre.marcio.ti@gmail.com>
2026-06-16 14:40:05 -0300

    verify task

--- main.py
+++ main.py
@@ -10,24 +10,31 @@ def main() -> None:
         players_data = json.load(f)
 
     for nickname, player_data in players_data.items():
-        race, _ = Race.objects.get_or_create(name=player_data["race"]["name"])
-
+        race, _ = Race.objects.get_or_create(
+            name=player_data["race"]["name"],
+            defaults={"description": player_data["race"]["description"]}
+        )
         guild_data = player_data.get("guild")
         if guild_data:
-            guild, _ = Guild.objects.get_or_create(name=guild_data["name"])
+            guild, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description")}
+            )
         else:
             guild = None
-        for skill in player_data.get("skills", []):
-            Skill.objects.get_or_create(name=skill["name"],
-                                        bonus=skill["bonus"],
-                                        race=race)
-
+        for skill in player_data["race"].get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
+            )
         Player.objects.create(
             nickname=nickname,
             email=player_data["email"],
             bio=player_data.get("bio", ""),
             race=race,
-            guild=guild)
+            guild=guild
+        )
 
 
 if __name__ == "__main__":
